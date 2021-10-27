1d6de2ba02fb22e45c9b9ae4d21d9eb698da4573
rhutuja3010 <91016307+rhutuja3010@users.noreply.github.com>
2021-10-27 11:07:11 +0530

    Add files via upload

---
+++ 10+something.py
@@ -0,0 +1,15 @@
+num=int(input("enter the number :"))
+i=0
+while i<1:
+    b=(num-10)
+    print(10,"+",b)
+    i+=1
+
+
+
+
+
+
+    
+
+    
---
+++ initial 10 .py
@@ -0,0 +1,36 @@
+# i=10
+# while i<20:
+#     a=i-9
+#     print(a)
+#     i+=1
+
+# i=1
+# while i<=100:
+#     if i%2==0:
+#         print(-i)
+#     else:
+#         print(i)
+#     i+=1
+
+
+
+sentence = "NavGurukul is an alternative to higher education reducing the barriers of current formal education system"
+# string=sentence.split( ) 
+# print(string)
+
+a=[]
+b=" "
+for i in sentence:
+    if i==" ":
+        a.append(b)
+        b=" "
+    else:
+        b+=i
+if b:
+    a.append(b)
+print(a)
+
+
+
+
+
