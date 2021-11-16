f3fab24b98c812055fb7f199eb3c2faf120f0afd
rhutuja3010 <91016307+rhutuja3010@users.noreply.github.com>
2021-11-16 14:32:56 +0530

    Add files via upload

---
+++ 1-100 reverse sum.py
@@ -0,0 +1,22 @@
+# i=1
+# sum=0
+# while i<=100:
+#     sum+=i
+#     i+=1
+#     print(sum)
+
+
+i=100
+sum=0
+while i>=1:
+    sum+=i
+    i-=1
+print(sum)
+
+
+# i=100
+# sum=5050
+# while i>=1:
+#     sum-=i
+#     i-=1
+#     print(sum)
